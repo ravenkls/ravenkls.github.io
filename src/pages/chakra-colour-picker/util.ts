5c5bfc1c0212037b2d914ad3326ed33632cca6a9
Kristian Smith <kristian.smith.2001@hotmail.co.uk>
2023-05-12 15:09:52 +0100

    proper colouring

--- src/pages/chakra-colour-picker/util.ts
+++ src/pages/chakra-colour-picker/util.ts
@@ -4,16 +4,16 @@ export const generateThemeColours = (colour: string) => {
   const baseColor = chroma(colour).hex();
 
   const colorScheme = {
-    50: chroma(baseColor).brighten(3).hex(),
-    100: chroma(baseColor).brighten(2.5).hex(),
-    200: chroma(baseColor).brighten(2).hex(),
-    300: chroma(baseColor).brighten(1.5).hex(),
-    400: chroma(baseColor).brighten(1).hex(),
-    500: chroma(baseColor).brighten(0.5).hex(),
-    600: baseColor,
-    700: chroma(baseColor).darken(0.5).hex(),
-    800: chroma(baseColor).darken(1).hex(),
-    900: chroma(baseColor).darken(1.5).hex(),
+    50: chroma(baseColor).brighten(2.5).hex(),
+    100: chroma(baseColor).brighten(2).hex(),
+    200: chroma(baseColor).brighten(1.5).hex(),
+    300: chroma(baseColor).brighten(1).hex(),
+    400: chroma(baseColor).brighten(0.5).hex(),
+    500: baseColor,
+    600: chroma(baseColor).darken(0.5).hex(),
+    700: chroma(baseColor).darken(1).hex(),
+    800: chroma(baseColor).darken(1.5).hex(),
+    900: chroma(baseColor).darken(2).hex(),
   };
 
   return colorScheme;
